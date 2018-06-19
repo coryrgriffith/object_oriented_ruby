1c90d1a0b9323ea7dbad67e92930401108830fd4
coryrgriffith <corygriffith9@gmail.com>
2018-06-18 20:30:54 -0500

    created store with 4 items

---
+++ store_item.rb
@@ -0,0 +1,9 @@
+store_item1 = {name: "pen", color: "blue", price: 1}
+store_item2 = {name: "eraser", color: "pink", price: 2}
+store_item3 = {name: "clock", color: "black", price: 5}
+store_item4 = {:name => "mouse", :color => "green", :price => 15}
+
+p "The #{store_item1[:name]} is #{store_item1[:color]} and costs $#{store_item1[:price]}."
+p "The #{store_item2[:name]} is #{store_item2[:color]} and costs $#{store_item2[:price]}."
+p "The #{store_item3[:name]} is #{store_item3[:color]} and costs $#{store_item3[:price]}."
+p "The #{store_item4[:name]} is #{store_item4[:color]} and costs $#{store_item4[:price]}."
